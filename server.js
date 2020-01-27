506438092c36db32a14c9f5d252c007024fab7b5
Clive Da <CliveDa@example.com>
2020-01-27 23:51:30 +0000

    autocommit

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const GLITCHON = process.env.GLITCHON ? 1 : 0
+const ZEITOFF = process.env.ZEITOFF ? 1 : 0
+const PORT = process.env.PORT ? process.env.PORT : 3000
+
 const URL = process.env.URL ? process.env.URL : 'https://www.charitychoice.co.uk/charities/human-rights/refugees/'
 const NUMPAGES = process.env.NUMPAGES ? process.env.NUMPAGES : 14
 
@@ -5,17 +9,17 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', async (_, res) => {
+app.get('*', async (_, res) => {
   console.info(new Date())
   page = Math.floor(Math.random() * NUMPAGES)
   res.redirect(URL+page)
 })
 
 
-// if (GLITCH) {
-//   app.listen(process.env.PORT ? process.env.PORT : 3000)
-// }
+if (GLITCHON) {
+  app.listen(PORT)
+}
 
-// if (ZEIT) {
-      module.exports = app
-// }
+if (!ZEITOFF) {
+  module.exports = app
+}
