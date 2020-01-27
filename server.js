3d401b21852126c141b1c7d7dd9c6261fe305832
Clive Da <CliveDa@example.com>
2020-01-27 23:28:16 +0000

    autocommit

---
+++ server.js
@@ -0,0 +1,14 @@
+const URL = process.env.URL ? process.env.URL : 'https://www.charitychoice.co.uk/charities/human-rights/refugees/'
+const NUMPAGES = process.env.NUMPAGES ? process.env.NUMPAGES : 14
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', async (_, res) => {
+  console.info(new Date())
+  page = Math.floor(Math.random() * NUMPAGES)
+  res.redirect(URL+page)
+})
+
+app.listen(process.env.PORT ? process.env.PORT : 3000)
