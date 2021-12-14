9bf09bd1893546bf86ad41583aaffdb06f0edd42
Clive D <osde8info@users.noreply.github.com>
2021-12-14 18:12:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+const GLITCHON = process.env.GLITCHON ? 1 : 0
+const ZEITOFF = process.env.ZEITOFF ? 1 : 0
+const PORT = process.env.PORT ? process.env.PORT : 3000
+
+const URL = process.env.URL ? process.env.URL : 'https://www.charitychoice.co.uk/charities/human-rights/refugees?pid='
+const NUMPAGES = process.env.NUMPAGES ? process.env.NUMPAGES : 25
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', async (_, res) => {
+  console.info(new Date())
+  page = Math.floor(Math.random() * NUMPAGES)
+  res.redirect(URL+page)
+})
+
+// are we running on GLITCH or ZEIT
+
+if (GLITCHON) {
+  app.listen(PORT)
+}
+
+if (!ZEITOFF) {
+  module.exports = app
+}
